4ab6da1532d339944656d2a81db7f10ab9b23b30
Gabriel <gabriel@qualyteam.com.br>
2019-03-07 15:31:32 -0300

    removed object

--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -8,47 +8,46 @@ namespace integrationPaddle.Controllers
 {
     public class PaddleBody
     {
-        string AlertName;
-        string BalanceCurrency;
-        string BalanceEarnings;
-        string BalanceFee;
-        string BalanceGross;
-        string BalanceTax;
-        string CheckoutId;
-        string Country;
-        string Coupon;
-        string Currency;
-        string CustomerName;
-        string Earnings;
-        string Email;
-        string EventTime;
-        string Fee;
-        string Ip;
-        string MarketingConsent;
-        string OrderId;
-        string Passthrough;
-        string PaymentMethod;
-        string PaymentTax;
-        string ProductId;
-        string ProductName;
-        string Quantity;
-        string ReceiptUrl;
-        string SaleGross;
-        string UsedPriceOverride;
-        string PSignature;
+        string AlertName { get; set; }
+        string BalanceCurrency { get; set; }
+        string BalanceEarnings { get; set; }
+        string BalanceFee { get; set; }
+        string BalanceGross { get; set; }
+        string BalanceTax { get; set; }
+        string CheckoutId { get; set; }
+        string Country { get; set; }
+        string Coupon { get; set; }
+        string Currency { get; set; }
+        string CustomerName { get; set; }
+        string Earnings { get; set; }
+        string Email { get; set; }
+        string EventTime { get; set; }
+        string Fee { get; set; }
+        string Ip { get; set; }
+        string MarketingConsent { get; set; }
+        string OrderId { get; set; }
+        string Passthrough { get; set; }
+        string PaymentMethod { get; set; }
+        string PaymentTax { get; set; }
+        string ProductId { get; set; }
+        string ProductName { get; set; }
+        string Quantity { get; set; }
+        string ReceiptUrl { get; set; }
+        string SaleGross { get; set; }
+        string UsedPriceOverride { get; set; }
+        string PSignature { get; set; }
     }
 
     [Route("api/[controller]")]
     [ApiController]
     public class WebhookController : ControllerBase
     {
-
-
         // POST api/values
         [HttpPost]
-        public IActionResult Post([FromBody] Object paddle)
+        public IActionResult Post()
         {
-            return Ok(paddle);
+            // [FromBody] Object paddle
+            return Ok(Request);
         }
 
     }
