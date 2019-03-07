8c096b2625ebea5028f3ebd6270a031de4d98096
Gabriel <gabriel@qualyteam.com.br>
2019-03-07 16:02:40 -0300

    testeeee

--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -9,35 +9,34 @@ namespace integrationPaddle.Controllers
 {
     public class PaddleBody
     {
-        IList AllKeys { get; set; }
-        // string AlertName { get; set; }
-        // string BalanceCurrency { get; set; }
-        // string BalanceEarnings { get; set; }
-        // string BalanceFee { get; set; }
-        // string BalanceGross { get; set; }
-        // string BalanceTax { get; set; }
-        // string CheckoutId { get; set; }
-        // string Country { get; set; }
-        // string Coupon { get; set; }
-        // string Currency { get; set; }
-        // string CustomerName { get; set; }
-        // string Earnings { get; set; }
-        // string Email { get; set; }
-        // string EventTime { get; set; }
-        // string Fee { get; set; }
-        // string Ip { get; set; }
-        // string MarketingConsent { get; set; }
-        // string OrderId { get; set; }
-        // string Passthrough { get; set; }
-        // string PaymentMethod { get; set; }
-        // string PaymentTax { get; set; }
-        // string ProductId { get; set; }
-        // string ProductName { get; set; }
-        // string Quantity { get; set; }
-        // string ReceiptUrl { get; set; }
-        // string SaleGross { get; set; }
-        // string UsedPriceOverride { get; set; }
-        // string PSignature { get; set; }
+        string AlertName { get; set; }
+        string BalanceCurrency { get; set; }
+        string BalanceEarnings { get; set; }
+        string BalanceFee { get; set; }
+        string BalanceGross { get; set; }
+        string BalanceTax { get; set; }
+        string CheckoutId { get; set; }
+        string Country { get; set; }
+        string Coupon { get; set; }
+        string Currency { get; set; }
+        string CustomerName { get; set; }
+        string Earnings { get; set; }
+        string Email { get; set; }
+        string EventTime { get; set; }
+        string Fee { get; set; }
+        string Ip { get; set; }
+        string MarketingConsent { get; set; }
+        string OrderId { get; set; }
+        string Passthrough { get; set; }
+        string PaymentMethod { get; set; }
+        string PaymentTax { get; set; }
+        string ProductId { get; set; }
+        string ProductName { get; set; }
+        string Quantity { get; set; }
+        string ReceiptUrl { get; set; }
+        string SaleGross { get; set; }
+        string UsedPriceOverride { get; set; }
+        string PSignature { get; set; }
     }
 
     [Route("api/[controller]")]
@@ -46,10 +45,10 @@ namespace integrationPaddle.Controllers
     {
         // POST api/values
         [HttpPost]
-        public IActionResult Post()
+        public IActionResult Post([FromForm] PaddleBody body)
         {
-            var dict = Request.Form.ToDictionary(x => x.Key, x => x.Value.ToString());
-            return Ok(dict);
+            // var dict = Request.Form.ToDictionary(x => x.Key, x => x.Value.ToString());
+            return Ok(body);
         }
 
     }
