799001b3f5160dfd824fa06825079b7477e0104b
Gabriel <gabriel@qualyteam.com.br>
2019-03-07 16:26:32 -0300

    xisde

--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -4,40 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace integrationPaddle.Controllers
 {
-    // public class PaddleBody
-    // {
-    //     string AlertName { get; set; }
-    //     string BalanceCurrency { get; set; }
-    //     string BalanceEarnings { get; set; }
-    //     string BalanceFee { get; set; }
-    //     string BalanceGross { get; set; }
-    //     string BalanceTax { get; set; }
-    //     string CheckoutId { get; set; }
-    //     string Country { get; set; }
-    //     string Coupon { get; set; }
-    //     string Currency { get; set; }
-    //     string CustomerName { get; set; }
-    //     string Earnings { get; set; }
-    //     string Email { get; set; }
-    //     string EventTime { get; set; }
-    //     string Fee { get; set; }
-    //     string Ip { get; set; }
-    //     string MarketingConsent { get; set; }
-    //     string OrderId { get; set; }
-    //     string Passthrough { get; set; }
-    //     string PaymentMethod { get; set; }
-    //     string PaymentTax { get; set; }
-    //     string ProductId { get; set; }
-    //     string ProductName { get; set; }
-    //     string Quantity { get; set; }
-    //     string ReceiptUrl { get; set; }
-    //     string SaleGross { get; set; }
-    //     string UsedPriceOverride { get; set; }
-    //     string PSignature { get; set; }
-    // }
+    public class PaddleBody
+    {
+        string AlertName { get; set; }
+        string BalanceCurrency { get; set; }
+        string BalanceEarnings { get; set; }
+        string BalanceFee { get; set; }
+        string BalanceGross { get; set; }
+        string BalanceTax { get; set; }
+        string CheckoutId { get; set; }
+        string Country { get; set; }
+        string Coupon { get; set; }
+        string Currency { get; set; }
+        string CustomerName { get; set; }
+        string Earnings { get; set; }
+        string Email { get; set; }
+        string EventTime { get; set; }
+        string Fee { get; set; }
+        string Ip { get; set; }
+        string MarketingConsent { get; set; }
+        string OrderId { get; set; }
+        string Passthrough { get; set; }
+        string PaymentMethod { get; set; }
+        string PaymentTax { get; set; }
+        string ProductId { get; set; }
+        string ProductName { get; set; }
+        string Quantity { get; set; }
+        string ReceiptUrl { get; set; }
+        string SaleGross { get; set; }
+        string UsedPriceOverride { get; set; }
+        string PSignature { get; set; }
+    }
 
     [Route("api/[controller]")]
     [ApiController]
@@ -47,16 +48,24 @@ namespace integrationPaddle.Controllers
         [HttpPost]
         public IActionResult Post()
         {
+            // JsonConvert.DeserializeObject<PaddleBody>(
             var dict = Request.Form.ToDictionary(x => x.Key, x => x.Value.ToString());
+            var teste = ObjectFromDictionary<PaddleBody>(dict);
             
-            // if(dict.alert_name === 'payment_succeeded'){
-                
-            // }
-            // if(dict.alert_name === 'payment_succeeded'){
-
-            // }
-            return Ok(dict);
+            return Ok(teste);
         }
+        private T ObjectFromDictionary<T>(IDictionary<string, string> dict)
+            where T : class 
+        {
+            Type type = typeof(T);
+            T result = (T)Activator.CreateInstance(type);
+            foreach (var item in dict)
+            {
+                type.GetProperty(item.Key).SetValue(result, item.Value, null);
+            }
+            return result;
+        }
+
         public string PaymentSuccess(string type){
             return "1";
         }
