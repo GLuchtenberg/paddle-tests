f6e4fcbbae440921c87e3912ffbe381a135910eb
Gabriel <gabriel@qualyteam.com.br>
2019-03-07 15:19:34 -0300

    Adicionado objeto

--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -6,15 +6,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace integrationPaddle.Controllers
 {
-    public class PaddleBody {
-
+    public class PaddleBody
+    {
+        string AlertName;
+        string BalanceCurrency;
+        string BalanceEarnings;
+        string BalanceFee;
+        string BalanceGross;
+        string BalanceTax;
+        string CheckoutId;
+        string Country;
+        string Coupon;
+        string Currency;
+        string CustomerName;
+        string Earnings;
+        string Email;
+        string EventTime;
+        string Fee;
+        string Ip;
+        string MarketingConsent;
+        string OrderId;
+        string Passthrough;
+        string PaymentMethod;
+        string PaymentTax;
+        string ProductId;
+        string ProductName;
+        string Quantity;
+        string ReceiptUrl;
+        string SaleGross;
+        string UsedPriceOverride;
+        string PSignature;
     }
 
     [Route("api/[controller]")]
     [ApiController]
     public class WebhookController : ControllerBase
     {
-        
+
 
         // POST api/values
         [HttpPost]
