d266945841c5f922c7e4e7d5b449d18a46f023c4
Gabriel <gabriel@qualyteam.com.br>
2019-03-07 15:04:00 -0300

    agora vai

---
+++ Controllers/WebhookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace integrationPaddle.Controllers
+{
+    public class PaddleBody {
+
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] PaddleBody paddle)
+        {
+            return Ok(paddle);
+        }
+
+    }
+}
