b398881b7c18110371f5e991b859bb7e5087330e
Gabriel <gabriel@qualyteam.com.br>
2019-03-08 07:47:00 -0300

    a

--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -11,34 +11,34 @@ namespace integrationPaddle.Controllers
 {
     public class PaddleBody
     {
-        string AlertName { get; set; }
-        string BalanceCurrency { get; set; }
-        string BalanceEarnings { get; set; }
-        string BalanceFee { get; set; }
-        string BalanceGross { get; set; }
-        string BalanceTax { get; set; }
-        string CheckoutId { get; set; }
-        string Country { get; set; }
-        string Coupon { get; set; }
-        string Currency { get; set; }
-        string CustomerName { get; set; }
-        string Earnings { get; set; }
-        string Email { get; set; }
-        string EventTime { get; set; }
-        string Fee { get; set; }
-        string Ip { get; set; }
-        string MarketingConsent { get; set; }
-        string OrderId { get; set; }
-        string Passthrough { get; set; }
-        string PaymentMethod { get; set; }
-        string PaymentTax { get; set; }
-        string ProductId { get; set; }
-        string ProductName { get; set; }
-        string Quantity { get; set; }
-        string ReceiptUrl { get; set; }
-        string SaleGross { get; set; }
-        string UsedPriceOverride { get; set; }
-        string PSignature { get; set; }
+        string alert_name { get; set; }
+        // string BalanceCurrency { get; set; }
+        // string BalanceEarnings { get; set; }
+        // string BalanceFee { get; set; }
+        // string BalanceGross { get; set; }
+        // string BalanceTax { get; set; }
+        // string CheckoutId { get; set; }
+        // string Country { get; set; }
+        // string Coupon { get; set; }
+        // string Currency { get; set; }
+        // string CustomerName { get; set; }
+        // string Earnings { get; set; }
+        // string Email { get; set; }
+        // string EventTime { get; set; }
+        // string Fee { get; set; }
+        // string Ip { get; set; }
+        // string MarketingConsent { get; set; }
+        // string OrderId { get; set; }
+        // string Passthrough { get; set; }
+        // string PaymentMethod { get; set; }
+        // string PaymentTax { get; set; }
+        // string ProductId { get; set; }
+        // string ProductName { get; set; }
+        // string Quantity { get; set; }
+        // string ReceiptUrl { get; set; }
+        // string SaleGross { get; set; }
+        // string UsedPriceOverride { get; set; }
+        // string PSignature { get; set; }
     }
 
     [Route("api/[controller]")]
@@ -46,37 +46,31 @@ namespace integrationPaddle.Controllers
     public class WebhookController : ControllerBase
     {
         List<Object> responses { get; set; }
-
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok(new { respostas = "ok", responses = this.responses });
-        }
         // POST api/values
         [HttpPost]
-        public IActionResult Post()
+        public IActionResult Post([FromForm] PaddleBody body)
         {
             // JsonConvert.DeserializeObject<PaddleBody>(
-            var dict = Request.Form.ToString();
-            responses.Add(dict);
-            JObject obj = JObject.Parse(dict);
-            PaddleBody body = obj.ToObject<PaddleBody>();
+            // var dict = Request.Form.ToString();
+            // responses.Add(dict);
+            // SortedDictionary<string, dynamic> padStuff = new SortedDictionary<string, dynamic>();
+            // JObject obj = JObject.Parse(dict);
+            // PaddleBody body = obj.ToObject<PaddleBody>();
             // var teste = ObjectFromDictionary<PaddleBody>(dict);
-
             return Ok(body);
         }
         // var dict = Request.Form.ToDictionary(x => x.Key, x => x.Value.ToString());
-        private T ObjectFromDictionary<T>(IDictionary<string, string> dict)
-            where T : class
-        {
-            Type type = typeof(T);
-            T result = (T)Activator.CreateInstance(type);
-            foreach (var item in dict)
-            {
-                type.GetProperty(item.Key).SetValue(result, item.Value, null);
-            }
-            return result;
-        }
+        // private T ObjectFromDictionary<T>(IDictionary<string, string> dict)
+        //     where T : class
+        // {
+        //     Type type = typeof(T);
+        //     T result = (T)Activator.CreateInstance(type);
+        //     foreach (var item in dict)
+        //     {
+        //         type.GetProperty(item.Key).SetValue(result, item.Value, null);
+        //     }
+        //     return result;
+        // }
 
         public string PaymentSuccess(string type)
         {
